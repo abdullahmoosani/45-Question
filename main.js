1482df6475ec691672e8ec1f0581353d3f6df7d0
abdullahmoosani <167894598+abdullahmoosani@users.noreply.github.com>
2024-04-26 22:52:28 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 var userName = 'Abdullah';
-console.log("hello ".concat(userName, " would you like to some typescript?"));
+// lower case
+console.log("lowercase:", userName.toLowerCase());
+// upper case
+console.log("uppercase:", userName.toUpperCase());
+//title case
+console.log("titlecase:", userName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
+//title case 
+console.log("titlecase:", userName.charAt(0).toUpperCase() + userName.slice(1).toLowerCase());
--- main.ts
+++ main.ts
@@ -1,2 +1,12 @@
 let userName: string = 'Abdullah';
-console.log(`hello ${userName} would you like to learn some typescript today?`);
+// lower case
+console.log("lowercase:", userName.toLowerCase());
+
+// upper case
+console.log("uppercase:", userName.toUpperCase());
+
+//title case
+console.log("titlecase:", userName.replace(/\bw/g,c=> c.toUpperCase()));
+
+//title case 
+console.log("titlecase:",userName.charAt(0).toUpperCase()+userName.slice(1).toLowerCase());
